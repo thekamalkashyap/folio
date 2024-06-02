269ae3fef1b905595232218a5ae38d79c0918fe7
thekamalkashyap <iamkamalkumar@proton.me>
2024-06-02 19:41:53 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Kamal Kashyap",
+  description: "Personal website of Kamal Kashyap",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={[inter.variable, GeistSans.variable].join(" ")}>
+      <body className="bg-black">{children}</body>
+    </html>
+  );
+}
